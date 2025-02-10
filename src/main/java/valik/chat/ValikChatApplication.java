240cfed9b1b1d9c538b255c177b4967d2b98b901
AndriiMariohlo <andrii.mariohlo@gmail.com>
2025-02-10 20:21:04 +0200

    Initialized spring project

---
+++ src/main/java/valik/chat/ValikChatApplication.java
@@ -0,0 +1,11 @@
+package valik.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValikChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ValikChatApplication.class, args);
+    }
+}
