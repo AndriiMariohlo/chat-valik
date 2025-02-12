a26f846e0a58df1badf38e6d1b846f92da44b0bd
AndriiMariohlo <andrii.mariohlo@gmail.com>
2025-02-12 18:24:39 +0200

    added sosal check

--- src/main/java/valik/chat/bot/ValikChatBot.java
+++ src/main/java/valik/chat/bot/ValikChatBot.java
@@ -9,7 +9,9 @@ import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.botapimethods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.reactions.SetMessageReaction;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.api.objects.reactions.ReactionTypeEmoji;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
@@ -18,17 +20,17 @@ import valik.chat.service.CitationParserService;
 import java.io.Serializable;
 import java.util.List;
 
-import static org.apache.commons.lang3.StringUtils.*;
+import static org.apache.commons.lang3.StringUtils.containsAnyIgnoreCase;
 
 @RequiredArgsConstructor
 @Slf4j
 @Component
 public class ValikChatBot implements LongPollingSingleThreadUpdateConsumer {
 
-    public static final String TEST_GROUP_ID = "-4620833741";
     private final TelegramClient telegramClient;
     private final CitationParserService citationParserService;
     private boolean justSent;
+    private Integer justSentMessageId;
 
     private void sendResponse(List<? extends PartialBotApiMethod<? extends Serializable>> botApiMethods) {
         log.debug("Total response messages size: {}", botApiMethods.size());
@@ -44,13 +46,18 @@ public class ValikChatBot implements LongPollingSingleThreadUpdateConsumer {
     }
 
     private void execute(PartialBotApiMethod<? extends Serializable> method) throws TelegramApiException {
-        if (method instanceof BotApiMethod<? extends Serializable> botApiMethod) {
+        if (method instanceof SendMessage sm) {
+            Message execute = telegramClient.execute(sm);
+            justSentMessageId = execute.getMessageId();
+        } else if (method instanceof BotApiMethod<? extends Serializable> botApiMethod) {
             telegramClient.execute(botApiMethod);
         }
     }
 
     @Override
     public void consume(Update update) {
+        checkSosal(update);
+
         int i = RandomUtils.secure().randomInt(0, 7);
         if (i == 6) {
             sendLikeEmoji(update);
@@ -63,10 +70,53 @@ public class ValikChatBot implements LongPollingSingleThreadUpdateConsumer {
         }
 
         if (update.getMessage().hasText()
-            && contains(update, "ебани", "ебанешь", "ебануть", "выкати", "выкатишь", "выкатить", "захуярь", "захуяришь", "захуярить", "выдави", "выдавишь", "выдавить")
+            && contains(update, "ебани", "ебанешь", "ебануть", "выкати", "выкатишь", "выкатить", "захуярь", "захуяришь", "захуярить", "выдави", "выдавишь", "выдавить", "скажи", "пиздани")
             && contains(update, "цитату", "цитатку", "цитаточку")
         ) {
-            sendMessages();
+            sendMessages(update.getMessage().getChatId());
+        }
+    }
+
+    private void checkSosal(Update update) {
+        checkSosalReply(update);
+        checkSosalNext(update);
+    }
+
+    private void checkSosalReply(Update update) {
+        Message replyToMessage = update.getMessage().getReplyToMessage();
+        if (replyToMessage != null) {
+            Boolean isBotReply = replyToMessage.getFrom().getIsBot();
+            Integer replyMessageId = replyToMessage.getMessageId();
+            boolean isYes = update.getMessage().getText().equalsIgnoreCase("да");
+
+            if (isBotReply && isYes) {
+                EditMessageText editMessage = EditMessageText.builder()
+                        .chatId(update.getMessage().getChatId())
+                        .messageId(replyMessageId)
+                        .text("Сосал?")
+                        .build();
+                SendMessage sm = SendMessage.builder()
+                        .chatId(update.getMessage().getChatId())
+                        .text("понял понял")
+                        .build();
+                sendResponse(List.of(editMessage, sm));
+            }
+        }
+    }
+
+    private void checkSosalNext(Update update) {
+        boolean isYes = update.getMessage().getText().equalsIgnoreCase("да");
+        if (justSent && isYes) {
+            EditMessageText editMessage = EditMessageText.builder()
+                    .chatId(update.getMessage().getChatId())
+                    .messageId(justSentMessageId)
+                    .text("Сосал?")
+                    .build();
+            SendMessage sm = SendMessage.builder()
+                    .chatId(update.getMessage().getChatId())
+                    .text("понял понял")
+                    .build();
+            sendResponse(List.of(editMessage, sm));
         }
     }
 
@@ -79,8 +129,8 @@ public class ValikChatBot implements LongPollingSingleThreadUpdateConsumer {
         return containsAnyIgnoreCase(update.getMessage().getText(), str);
     }
 
-    public void sendMessages() {
-        SendMessage response = SendMessage.builder().text(citationParserService.getRandomCitation()).chatId(TEST_GROUP_ID).build();
+    public void sendMessages(Long chatId) {
+        SendMessage response = SendMessage.builder().text(citationParserService.getRandomCitation()).chatId(chatId).build();
         sendResponse(List.of(response));
         log.debug("Message sent: {}", response.getText());
         justSent = true;
